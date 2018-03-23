e50dd5c0723060401056de45b8af78901f254567
Unknown <jean.auquier@live.fr>
2018-03-23 10:12:30 +0100

    Among The Tests - Realistic Water (Bad)

---
+++ AmongTheWave/Assets/Scripts/Camera/AutoCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCamera : MonoBehaviour {
+
+	public Transform target;
+
+	[Header("Camera Parameters")]
+	public Vector3 offset;
+
+	[Header("Tweaking")]
+	public float lerpForce;
+
+	[Header("References")]
+	public GameObject actualCamera;
+
+	Camera _cam;
+
+	//Constante
+	float leniencyLimit = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		_cam = actualCamera.GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		Vector3 mov = (target.position + offset) - transform.position;
+		mov.y = 0;
+
+		if (mov.magnitude > leniencyLimit) {
+			float movMagn = Mathf.Lerp (0, mov.magnitude, lerpForce * Time.fixedDeltaTime);
+			transform.position += mov.normalized * movMagn;
+		}
+	}
+}
---
+++ AmongTheWave/Assets/Scripts/WaterPhysics/Stream.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stream : MonoBehaviour {
+
+	public static Stream current;
+
+	[SerializeField]
+	Vector2 streamDirection;
+	[SerializeField]
+	float streamForce;
+
+	public Vector3 MainStream {
+		get {
+			return new Vector3(streamDirection.x, 0, streamDirection.y).normalized * streamForce;
+		}
+	}
+
+	void Start() 
+	{
+		current = this;
+	}
+}
