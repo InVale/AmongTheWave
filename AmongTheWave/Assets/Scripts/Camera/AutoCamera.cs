ab99b70898f346dbb437cd86d737370dbbd6f784
Unknown <jean.auquier@live.fr>
2018-03-24 20:07:07 +0100

    Boat Controller, started to work on Camera.

--- AmongTheWave/Assets/Scripts/Camera/AutoCamera.cs
+++ AmongTheWave/Assets/Scripts/Camera/AutoCamera.cs
@@ -6,28 +6,46 @@ public class AutoCamera : MonoBehaviour {
 
 	public Transform target;
 
-	[Header("Camera Parameters")]
-	public Vector3 offset;
+	//[Header("Camera Parameters")]
 
-	[Header("Tweaking")]
-	public float lerpForce;
+	[Header("Camera Tweaking")]
+	public Vector2 turnLimit;
+	public float cameraSpeed;
+	public AnimationCurve turnEase;
 
 	[Header("References")]
 	public GameObject actualCamera;
 
 	Camera _cam;
+	float currentX;
+	float currentY;
 
-	//Constante
-	float leniencyLimit = 0.05f;
-
-	// Use this for initialization
 	void Start () {
 		_cam = actualCamera.GetComponent<Camera> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		float xAxis = Input.GetAxis ("Camera Horizontal");
+		float yAxis = Input.GetAxis ("Camera Vertical");
+
+		currentX = MoveAxis (xAxis, currentX);
+		currentY = MoveAxis (yAxis, currentY);
+
+		float x = Mathf.Sign (currentX) * turnEase.Evaluate (Mathf.Abs (currentX)) * turnLimit.x;
+		float y = Mathf.Sign (currentY) * turnEase.Evaluate (Mathf.Abs (currentY)) * turnLimit.y;
+
+		actualCamera.transform.localEulerAngles = new Vector3(y, x, 0);
+	}
+
+	float MoveAxis (float axis, float current) {
+		if (axis == 0 && current == 0)
+			return 0;
+
+		float dir = (axis > currentX) ? 1 : -1;
+		current += dir * cameraSpeed * Time.deltaTime;
+		float pos = (axis > currentX) ? 1 : -1;
+		current = (pos != dir) ? axis : current;
+		return current;
 	}
 
 	void FixedUpdate () {
--- AmongTheWave/Assets/Scripts/NewWaterPhysics/Boat.cs
+++ AmongTheWave/Assets/Scripts/NewWaterPhysics/Boat.cs
@@ -1,17 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class Boat : MonoBehaviour {
 
-	public Vector2 current;
-	public float accelerationFactor;
+	//Debug
+	[Header("Debug")]
+	[ReadOnly, SerializeField]
+	float currentVelocity;
+	[ReadOnly, SerializeField]
+	float currentAcceleration = 1;
+	[ReadOnly, SerializeField]
+	float currentTurnSpeed;
+	[ReadOnly, SerializeField]
+	float currentTurnDrag;
 
+	//Public
+	[Header("Tweak")]
+	public Vector2 current;
+	public float size;
+	public float accelerationFactor;
+	[Space]
+	public float turnSpeed;
+	public float turnDrag;
+	public float rudderSpeed;
+	[Space]
+	public TurnCalculTypeEnum turnFactorCalcul;
+	public float turnFactor;
+	public float turnDragFactor;
+	public float referenceVelocity;
+
+	[Header("References")]
+	public Transform rudder;
+
+	//Internal
 	Rigidbody _rb;
 	Animator _anim;
 
 	bool _immobile = false;
-	bool _accelerate = false;
+	float _currentDir = 0;
+
+	public enum TurnCalculTypeEnum {
+		ADDITIVE,
+		MULTIPLICATIVE
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -25,21 +58,66 @@ public class Boat : MonoBehaviour {
 		if (Input.GetButtonDown ("Fire1"))
 			_immobile = !_immobile;
 
-		if (Input.GetButton ("Accelerate"))
-			_accelerate = true;
-		else
-			_accelerate = false;
+		if (Input.GetButtonDown ("Accelerate"))
+			currentAcceleration += accelerationFactor;
 
+		if (Input.GetButtonDown("Cancel")) {
+			currentAcceleration = 1;
+		}
 
+		RudderDirection ();
+	}
+
+	void RudderDirection () {
+		float input = Input.GetAxis ("Horizontal") * -45;
+		float dir = input - _currentDir;
+
+		if (dir != 0) {
+			dir = Mathf.Sign (dir);
+			_currentDir += dir * rudderSpeed * Time.deltaTime;
+			_currentDir = (Mathf.Abs(_currentDir) > 45) ? dir * 45 : _currentDir;
+			_currentDir = ((input == 0) && (Mathf.Abs (_currentDir) < 0.15f)) ? 0 : _currentDir;
+			rudder.localEulerAngles = Vector3.up * _currentDir;
+		}
 	}
 
 	void FixedUpdate () {
+		Vector3 rot = transform.eulerAngles;
+		rot.x = 0;
+		rot.z = 0;
+		transform.eulerAngles = rot;
+
 		if (!_immobile) {
+			//Calculate Trajectory
 			Vector3 mov = new Vector3(current.x, 0, current.y);
-			mov = (_accelerate) ? mov * accelerationFactor : mov;
-			_rb.AddForce (mov);
+			mov *= size * currentAcceleration;
+
+			//Calculate Turn Factor
+			float tFactor = 1;
+			float dFactor = 1;
+			if (turnFactorCalcul == TurnCalculTypeEnum.ADDITIVE) {
+				float factor = _rb.velocity.magnitude / referenceVelocity;
+				tFactor += factor * turnFactor;
+				if (tFactor <= 0)
+					Debug.Log("Your turnFactor value is too negative for additive calcul considering the reachable speed, factor ended up being negative.");
+				dFactor += factor * turnDragFactor;
+			}
+			else if (turnFactorCalcul == TurnCalculTypeEnum.MULTIPLICATIVE) {
+				tFactor = Mathf.Pow (turnFactor, (_rb.velocity.magnitude / referenceVelocity));
+				dFactor = Mathf.Pow (turnDragFactor, (_rb.velocity.magnitude / referenceVelocity));
+			}
+				
+			//Add Forces
+			currentTurnSpeed = tFactor * turnSpeed * Input.GetAxis ("Horizontal");
+			_rb.AddTorque (Vector3.up * currentTurnSpeed);
+			_rb.AddForce (mov.magnitude * transform.forward);
+		
+			currentTurnDrag = dFactor * turnDrag;
+			_rb.angularDrag = currentTurnDrag;
 		}
 
+		currentVelocity = _rb.velocity.magnitude;
 		//_anim.SetFloat("ForwardSpeed")
+
 	}
 }
