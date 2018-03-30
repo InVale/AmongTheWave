3fe7efe1d36811526391f68d97780390ec0afac4
kuarisu <kuarisuran@gmail.com>
2018-03-30 15:31:45 +0200

    LEVEL DESIGN PROTO

---
+++ AmongTheWave/Assets/Scripts/Temporary/StartCinematic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class StartCinematic : MonoBehaviour {
+
+    public PlayableDirector timeline;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            timeline.Play();
+        }
+    }
+}
