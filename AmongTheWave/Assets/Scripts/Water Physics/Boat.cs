c6df3795b9edf8553db7e3681e17081a597e6660
Unknown <jean.auquier@live.fr>
2018-04-09 19:15:28 +0200

    Commented Boat's code. PROJECT CLOSED.

--- AmongTheWave/Assets/Scripts/Water Physics/Boat.cs
+++ AmongTheWave/Assets/Scripts/Water Physics/Boat.cs
@@ -4,150 +4,211 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 using DarkTonic.MasterAudio;
 
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(FloatingObjectAnimations))]
+//Hey Clarisse, content de voir que t'as envie de reprendre Among the Wave.
+//J'ai donc commenté l'intégralité du code de contrôle du bateau ainsi que du code d'animation (FloatingObjectAnimation) afin de te donner un coup de main.
+//Je n'ai commenté que c'est deux là puisque c'est là que ya eu tout l'engineering côté prog.
+//Surtout n'hésite pas à me contacter si t'as la moindre question ou si tu veux un coup de main.
+
+//Bon on commence direct avec ces deux trucs là.
+//En gros je me sentais un peu trop chaud et j'ai décidé de faire les truc de façon propre (à peu près).
+[RequireComponent(typeof(Rigidbody))] //Rigidbody obligatoire vu qu'on build par dessus.
+[RequireComponent(typeof(FloatingObjectAnimations))] //Et le code gérant les animations du bateau, mandatory aussi.
 public class Boat : MonoBehaviour {
 
-	//Debug
-	[FoldoutGroup("Debug")]
+	//DEBUG
+	//(Tout à été mis sous FoldoutGroup("Debug") qui est un truc d'Odin qui permet de les regrouper.
+	//C'est des valeurs pratiques à avoir pour du debug mais qui ne sont pas du tweaking en soit.
+	//D'ailleurs, la plupart sont en ReadOnly (un truc d'Odin assez explicite) et en SerializeField pour apparaitre dans l'inspecteur en restant privée.
+	[FoldoutGroup("Debug"), Tooltip("Permet de setup la direction et la force du courant affectant le bateau en attendant que le système soit fait autrement et que le bateau récupère lui même cette valeur.")]
 	public Vector2 stream;
-	[FoldoutGroup("Debug")]
+	[FoldoutGroup("Debug"), Tooltip("Permet d'immobiliser le bateau pour du Debug et le début de partie.")]
 	public bool immobile;
 
-	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Space]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Vélocité du bateau."), Space]
 	float currentVelocity;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
-	float currentAcceleration = 1;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Niveau d'accélération de Debug.")]
+	float currentAcceleration = 1; //Relatif au système d'accélération pour le debug, voir en dessous.
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Vélocité angulaire du bateau.")]
 	float currentTurnSpeed;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
-	float currentTurnDrag;
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Angular Drag du rigidbody bateau.")]
+	float currentTurnDrag; //Cette valeur est aussi visible dans le Rigidbody puis qu'on l'applique à l'Angular Drag du Rigidbody, mais je l'ai remis là pour rassembler les trucs.
 
-	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Space]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Angle entre le forward du bateau et le sens du courant."), Space]
 	float angleToStream;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Valeur d'angle maximale autorisée pour Angle To Stream.")]
 	float currentMaxAngle;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Quotient de Angle To Stream divisé par Current Max Angle. Sert à opposer une force de résistance à la rotation angulaire.")]
 	float rFactor;
 
-	//Public
-	[Header("Tweak")]
-	public float size;
-	public float accelerationFactor;
+
+	//PUBLIC
+	[Header("Tweak"), Tooltip("Volume du bateau. Permet de faire les calculs de force s'y appliquant pour fonctionner en paire avec la valeur de masse dans le Rigidbody.")]
+	//En gros l'idée c'est que plus un object est gros, plus il offre de prise au courant et donc plus de force s'y applique.
+	//Mais le problème c'est qu'en général, plus il est gros et plus il est lourd et donc plus il faut de force pour le faire bouger.
+	//Normalement pour modéliser cette prise au courant j'aurais du faire un truc stylé en regardant le mesh.
+	//Mais comme j'avais pas de mesh définitif et que j'avais la flemme, j'ai mobilisé ce truc à la shlag en mettant une valeur de Size qui vient multiplier la force du courant.
+	//Cette augmentation de la force du courant est nécessaire car on set la masse dans le rigidbody pour les collisions et qu'à cause de ça, il faut bien plus de force pour atteindre la même vélocité.
+	public float size; 
+	[Tooltip("De combien le niveau d'accélération est augmenté à chaque input d'accélération (RB). La valeur de courant est multiplié par la niveau d'accélération.")]
+	public float accelerationFactor; //Ce truc d'accélération c'est strictement que du debug.
+	[Tooltip("Peut-on accélérer/deccélérer en utilisant l'axe Y du Joystick gauche ?")]
 	public bool yControl;
-	[ShowIf("yControl")]
+	[ShowIf("yControl")] //Fonction d'Odin super pratique permettant de cacher/montrer une variable dans l'inspecteur en fonction de l'état d'une variable booléenne.
+	[Tooltip("Par combien la force du courant est multiplié quand le joystick est vers le bas (freinage).")]
 	public float brakeFactor;
-	[ShowIf("yControl")]
+	[ShowIf("yControl"), Tooltip("Par combien la force du courant est multiplié quand le joystick est vers le haut (accélération).")]
 	public float speedFactor;
-	[Space]
+
+	[Tooltip("Force initiale de rotation angulaire."), Space]
 	public float turnSpeed;
+	[Tooltip("Drag Angulaire initiale du Rigidbody. Override la valeur d'Angular Drag du Rigidbody.")]
 	public float turnDrag;
+	[Tooltip("Vitesse de rotation du gouvernail à l'arrière du bateau. Purement cosmétique.")]
 	public float rudderSpeed;
-	[Space]
+
+	[Tooltip("Comment est-ce que l'évolution des valeurs de Turn Speed et Turn Drag en fonction de la vitesse sera calculé ? Par multiplication ou addition du facteur ?"), Space]
 	public TurnCalculTypeEnum turnFactorCalcul;
+	[Tooltip("Facteur d'augmentation de la force de rotation avec la vitesse.")]
 	public float turnFactor;
+	[Tooltip("Facteur d'augmentation de l'angular drag du rigidbody avec la vitesse.")]
 	public float turnDragFactor;
+	[Tooltip("Valeur de référence de vélocité pour faciliter les calculs d'évolution des valeurs avec la vitesse.")]
 	public float referenceVelocity;
-	[Space]
+
+	[Tooltip("Comment est-ce que l'évolution de la valeur d'angle maximale de rotation en fonction de la vitesse sera calculé ? Par multiplication ou addition du facteur ?"), Space]
 	public TurnCalculTypeEnum turnAngleCalcul;
+	[Tooltip("Valeur initiale d'angle maximale de rotation.")]
 	public float turnMaxAngle;
+	[Tooltip("Facteur d'augmentation (ou diminution en fonction de la valeur) de la valeur d'angle maximal de rotation avec la vitesse.")]
 	public float turnAngleFactor;
 
-	//[Header("Sound")]
-
-	[Header("References")]
+	[Header("References"), Tooltip("Reference du Transform du gouvernail.")]
 	public Transform rudder;
-	[SoundGroup]
+	[SoundGroup] //ça c'est lié au plugin audio. C'est dégueux, oublie.
 	public string boatCreakSound;
 
-	//Internal
-	Rigidbody _rb;
-	FloatingObjectAnimations _floatAnim;
+	//INTERNAL
+	Rigidbody _rb; //ref du rigidbody.
+	FloatingObjectAnimations _floatAnim; //ref du script d'anim de flottaison.
 
-	float _currentDir = 0;
-	float _lastInput = 0;
-	float _lastInputTime;
+	float _currentDir = 0; //Angle actuel du gouvernail.
+	float _lastInput = 0; //Dernière position du stick pour le son de craquement.
+	float _lastInputTime; //Cooldon pour le son de craquement.
 
+	//Enum pour les différents mode de calcul
 	public enum TurnCalculTypeEnum {
 		ADDITIVE,
 		MULTIPLICATIVE
 	}
-
-	// Use this for initialization
+		
+	//Assez classique, on setup les refs du rigidbody et du script d'anim qu'on est d'ailleurs sur d'avoir grâce au RequireComponent.
 	void Start () {
 		_rb = GetComponent<Rigidbody> ();
 		_floatAnim = GetComponent<FloatingObjectAnimations> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
+		//Avec Start on peut immobiliser/remettre en marche le bateau. (DEBUG)
 		if (Input.GetButtonDown ("Start")) {
 			immobile = !immobile;
 		}
 
+		//On augmente le niveau d'accélération si on reçoit l'input (RB). (DEBUG)
 		if (Input.GetButtonDown ("Accelerate"))
 			currentAcceleration += accelerationFactor;
 
+		//On reset le niveau d'accélération si on reçoit l'input (B). (DEBUG)
 		if (Input.GetButtonDown("Cancel")) {
 			currentAcceleration = 1;
 		}
-
+			
+		//On appelle les mouvements du gouvernail.
 		RudderDirection ();
 	}
 
+	//Fonction gérant le gouvernail.
 	void RudderDirection () {
-		float input = Input.GetAxis ("Horizontal") * -45;
-		float dir = input - _currentDir;
+		float input = Input.GetAxis ("Horizontal") * -45; //Le 45 c'est l'angle maximal du gouvernail hardcodé. C'est -45 car le gouvernail doit rotate dans le sens opposé au sens désiré.
+		float dir = input - _currentDir; //On calcul l'angle entre la rotation désirée par rapport à la position du stick et la rotation actuelle du gouvernail.
 
-		if (dir != 0) {
+		if (dir != 0) { //Si l'angle obtenue est différent de 0, alors un mouvement est nécessaire.
+
+			//On rotate le gouvernail dans la bonne direction par la vitesse de rotation * Time.deltaTime.
 			dir = Mathf.Sign (dir);
 			_currentDir += dir * rudderSpeed * Time.deltaTime;
+
+			//On cap l'angle à 45 s'il est supérieur à cette valeur.
 			_currentDir = (Mathf.Abs(_currentDir) > 45) ? dir * 45 : _currentDir;
+
+			//ça c'est sensé évité que le gouvernail tremble lorsqu'il est à 0, mais ça marche moyen et j'ai eu la flemme de le recoder.
 			_currentDir = ((input == 0) && (Mathf.Abs (_currentDir) < 0.15f)) ? 0 : _currentDir;
+
+			//On change la rotation du gouvernail pour correspondre à la nouvelle valeur.
 			Vector3 rudRot = rudder.localEulerAngles;
 			rudRot.y = _currentDir;
 			rudder.localEulerAngles = rudRot;
 		}
 	}
 
+	//On fait les opérations de physiques dans le FixedUpdate
 	void FixedUpdate () {
+
+		//On ne fait aucune opération si le bateau doit être immobile.
 		if (!immobile) {
+
+			//On appelle la fonction s'occupant de faire le gros des opérations de rotation du bateau.
 			Rotation ();
 
+			//Code rapide pour le système d'accélération/deccélération en fonction de l'axe Y du Joystick gauche.
 			float factor = 1;
 			if (yControl) {
 				factor = Input.GetAxis ("Vertical");
 				factor = (factor < 0) ? brakeFactor : ((factor > 0) ? speedFactor : 1);
 			}
 
+			//On applique la force du courant au bateau pour le mouvoir.
+			//La force s'applique en fonction du forward du bateau, ballec du sens du courant :^)
+			//Enfin en vrai faudrait corriger ça pour quand on prend en coup.
+			//Mais en temps normal le modèle marche bien vu que le forward du bateau est limité par le sens du courant.
 			Vector3 mov = new Vector3(stream.x, 0, stream.y);
 			mov *= size * currentAcceleration;
 			_rb.AddForce (mov.magnitude * transform.forward * factor);
 		}
-			
+
+		//La variable indiquant la vélocité du bateau est update même quand il est immobile.
 		currentVelocity = _rb.velocity.magnitude;
 	}
 
+	//Fonction s'occupant de la rotation du bateau.
 	void Rotation () {
+		//Input Horizontal qu'on garde en ref.
 		float xInput = Input.GetAxis ("Horizontal");
+		//Facteur de vitesse pour les calculs.
 		float factor = _rb.velocity.magnitude / referenceVelocity;
 
-		//Calculate Turn Factor
+		//On calcul d'abord le facteur pour la force de rotation et la valeur d'angular drag pour les faire évoluer avec la vitesse.
 		float tFactor = 1;
 		float dFactor = 1;
+		//En additif, facteur = 1 + (facteur de vitesse) * (valeur de facteur).
 		if (turnFactorCalcul == TurnCalculTypeEnum.ADDITIVE) {
 			tFactor += factor * turnFactor;
 			if (tFactor <= 0)
+				//L'idée de ce debug log, c'est au cas où une valeur négative de facteur est usité.
+				//Si la valeur ou la vitesse est suffisament grande, au lieu d'une simple diminution, on peut obtenir un facteur négatif, donnant des résultats bizarres.
 				Debug.Log("Your turnFactor value is too negative for additive calcul considering the reachable speed; factor ended up being negative.");
 			dFactor += factor * turnDragFactor;
 		}
+		//En multiplicatif, facteur = (valeur de facteur) ^ (facteur de vitesse).
 		else if (turnFactorCalcul == TurnCalculTypeEnum.MULTIPLICATIVE) {
 			tFactor = Mathf.Pow (turnFactor, factor);
 			dFactor = Mathf.Pow (turnDragFactor, factor);
+			//Pas besoin de check dans ce cas, il n'est pas possible d'obtenir un facteur négatif "involontairement".
 		}
 
-		//Calculate Water Resistance
+		//On calcule ensuite l'angle maximale de rotation en fonction de la vitesse.
+		//La valeur d'angle vas servir à calculer la résistance à la rotation du bateau pour faire un truc smooth.
+		//Le mode de calcul est différent au cas où.
 		if (turnAngleCalcul == TurnCalculTypeEnum.ADDITIVE) {
 			float buffer = 1 + factor * turnAngleFactor;
 			if (tFactor <= 0)
@@ -157,21 +218,31 @@ public class Boat : MonoBehaviour {
 		else if (turnAngleCalcul == TurnCalculTypeEnum.MULTIPLICATIVE) {
 			currentMaxAngle = turnMaxAngle * Mathf.Pow (turnAngleFactor, factor);
 		}
+		//Une fois qu'on a l'angle max, on choppe l'angle du bateau par rapport au courant.
+		//On choppe l'angle avec son signe afin d'avoir le sens dans lequel appliqué la résistance.
 		angleToStream = Vector2.SignedAngle(stream.normalized, new Vector2(transform.forward.x, transform.forward.z));
+		//Et on obtient ainsi le facteur de résistance.
 		rFactor = angleToStream / currentMaxAngle;
 
-		//Add Forces
+		//On ajoute enfin les forces.
+		//La force de rotation dépend ainsi du facteur de rotation (qui dépend de la vitesse), de l'input horizontal, et de la force de résistance qui essaye de ramener le bateau dans le droit chemin.
+		//L'angular drag de base du bateau couplé à la résistance permet d'éviter de (trop) dépasser l'angle maximal de rotation du bateau.
 		currentTurnSpeed = tFactor * turnSpeed * (xInput + rFactor);
-		_rb.AddTorque (_floatAnim.boatsNormal * currentTurnSpeed);
-		_floatAnim.SetSideMovement (tFactor * turnSpeed * xInput);
+		_rb.AddTorque (_floatAnim.boatsNormal * currentTurnSpeed); //On applique la force par rapport à la normal du bateau qui aurait ultimement dépendu des vagues.
+		_floatAnim.SetSideMovement (tFactor * turnSpeed * xInput); //On balance une valeur au script d'anim' afin qu'il est des infos pour gérer l'inclinaison du bateau sur l'axe z.
 
+		//On change la valeur d'angular drag calculé en fonction de la vitesse.
+		//ça permet d'avoir un bateau plus réactif plus on va vite (moins d'inertie).
 		currentTurnDrag = dFactor * turnDrag;
 		_rb.angularDrag = currentTurnDrag;
 
-		//Play Sound
+		//Le truc qui gère les craquements.
+		//On check d'abord si le stick est dans une position "absolue" (full gauche, full droite, neutre).
 		if (xInput == 1 || xInput == -1 || xInput == 0) {
+			//Si oui, on check si le cooldown est fini et si la position est différente de la dernière position enregistré.
 			if (xInput != _lastInput && _lastInputTime <= Time.time) {
-				_lastInputTime = Time.time + 1f;
+				//Auquel cas on joue le son.
+				_lastInputTime = Time.time + 1f; //Ce truc là c'est une manière stylé de faire un cooldown. En gros tu choppes le Time.time du jeu et tu lui ajoutes une valeur. Puis tu attends que ton Time.time lui soit supérieur.
 				MasterAudio.PlaySound3DAtTransformAndForget(boatCreakSound, transform);
 			} 
 			_lastInput = xInput;
--- AmongTheWave/Assets/Scripts/Water Physics/FloatingObjectAnimations.cs
+++ AmongTheWave/Assets/Scripts/Water Physics/FloatingObjectAnimations.cs
@@ -5,120 +5,158 @@ using Sirenix.OdinInspector;
 using System;
 using DarkTonic.MasterAudio;
 
+//Hey, si tu l'as pas déjà vu, tu ferais mieux de d'abord check le script Boat !
+
+//J'avais écrit ce script en me disant qu'à terme il ferait partie d'une série de script basé sur l'héritage qui permetterait de gérer le fonctionnement de tout les objets flottant.
+//Mais j'ai fait des trucs un peu crade en fait.
+
 [RequireComponent(typeof(Rigidbody))]
 public class FloatingObjectAnimations : MonoBehaviour {
 
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	//DEBUG
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Facteur de blend entre les animations normales et les animations à haute vitesse. Basé sur la vitesse du bateau.")]
 	float currentSpeedFactor;
 
-	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Space]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Valeur de position en y du bateau dans son animation de flottaison."), Space]
 	float currentBuoyancyValue;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Valeur indiquant la progression dans l'animation de flottaison (allant de 0 à 1).")]
 	float currentBuoyancyAnimationPoint;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Amplitude de l'animation de flottaison.")]
 	float currentBuoyancyStrength;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Durée d'un cycle de l'animation de flottaison.")]
 	float currentBuoyancyLength;
 
-	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Space]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Valeur de rotation en x dans l'animation d'inclinaison du bateau du à la vitesse."), Space]
 	float currentForwardTiltAngle;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Amplitude de l'animation d'inclinaison du bateau.")]
 	float currentForwardTiltStrength;
 
-	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Space]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Valeur de rotation en z dans l'animation de tanguage du bateau."), Space]
 	float currentSideAngle;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Valeur d'angle d'inclinaison du bateau recherchée.")]
 	float aimedSideAngle;
-	[ReadOnly, SerializeField, FoldoutGroup("Debug")]
+	[ReadOnly, SerializeField, FoldoutGroup("Debug"), Tooltip("Vélocité d'inclinaison du bateau en z.")]
 	float currentSideAngularVelocity;
 
-	[Header("Basic Buoyancy")]
+	//PUBLIC
+	[Header("Basic Buoyancy"), Tooltip("Amplitude de l'animation de flottaison au niveau 0 de blend.")]
 	public float restStrength;
+	[Tooltip("Durée d'un cycle de l'animation de flottaison au niveau 0 de blend.")]
 	public float restLength;
+	[Tooltip("Courbe de l'évolution de la position en y du bateau pour l'animation de flottaison. La durée de la courbe doit être de 1 unité.")]
 	public AnimationCurve restCurve;
 
-	[Header("High Speed Buoyancy")]
+	[Header("High Speed Buoyancy"), Tooltip("Vitesse à laquelle le blend des animations doit être égal à 1. Autrement dit, valeur de vitesse à laquelle le bateau est considéré comme \"allant vite\".")]
 	public float fastSpeedReference;
-	[Space]
+
+	[Tooltip("Amplitude de l'animation de flottaison au niveau 1 de blend."), Space]
 	public float fastSpeedStrenght;
+	[Tooltip("Durée d'un cycle de l'animation de flottaison au niveau 1 de blend.")]
 	public float fastSpeedLenght;
-	[Space]
+
+	[Tooltip("Amplitude de l'animation d'inclinaison au niveau 1 de blend."), Space]
 	public float fastSpeedTiltStrength;
+	[Tooltip("Courbe de l'évolution de la rotation en x du bateau pour l'animation d'inclinaison du bateau du à la vitesse. La durée de la courbe doit être de 1 unité.")]
 	public AnimationCurve fastSpeedTiltCurve;
 
-	[Header("Side Tilt")]
+	[Header("Side Tilt"), Tooltip("Amplitude de l'animation de tanguage en fonction de la vitesse.")]
 	public float sideStrength;
+	[Tooltip("Vitesse de tanguage du bateau pour atteindre l'angle désiré.")]
 	public float sideSpeed;
+	[Tooltip("Vitesse de tanguage du bateau quand il revient au point neutre.")]
 	public float sideGravity;
+	[Tooltip("Drag sur la vélocté du tanguage du bateau (vitesse à laquelle la vélocité du tanguage du bateau diminue).")]
 	public float sideDrag;
 
 	[Header("Sound"), SoundGroup]
-	public string waterSound;
+	public string waterSound; //C'est lié au plugin audio, oublie
+	[Tooltip("Courbe d'évolution du volume des bruitages d'eau en fonction de la vitesse. La durée de la courbe doit être de 1 unité.")]
 	public AnimationCurve waterVolume;
+	[Tooltip("Quand est-ce que le son doit être joué dans le cycle d'animation du bateau. Doit être compris entre 0 et 1.")]
 	public float waterTimeStamp;
+
 	[Space]
-	public AudioSource waterAmbient;
+	public AudioSource waterAmbient; //C'est lié au plugin audio, oublie
+	[Tooltip("Amplitude des sons d'eau.")]
 	public float waterAmbientVolumeMultiplier;
 
+	//Internal
 	Rigidbody _rb;
-
-	[NonSerialized]
-	public Vector3 boatsNormal = Vector3.up;
-
-	bool _waterCanPlay = true;
+	[NonSerialized] //Pratique quand tu veux une variable accessible depuis l'extérieur mais qui n'a pas besoin d'être serialisé/apparaitre dans l'inspecteur.
+	public Vector3 boatsNormal = Vector3.up; //Une constante en attendant de recup la normal aux vague.
+	bool _waterCanPlay = true; //Un buffer pour jouer le son d'eau uniquement à certains points de l'anim'.
 
 	void Start () {
 		_rb = GetComponent<Rigidbody> ();
 	}
 
 	void Update () {
+		//Dans l'update on met le volume du son d'eau au bon niveau en fonction de la vitesse.
 		waterAmbient.volume = waterVolume.Evaluate ((_rb.velocity.magnitude > fastSpeedReference) ? 1 : _rb.velocity.magnitude / fastSpeedReference) * waterAmbientVolumeMultiplier;
 	}
 
 	void FixedUpdate () {
+		//On calcul le facteur de vitesse. Le pragma permet de caper le facteur à 1.
 		currentSpeedFactor = _rb.velocity.magnitude / fastSpeedReference;
 		currentSpeedFactor = (currentSpeedFactor > 1) ? 1 : currentSpeedFactor;
 
+		//On appelle l'anim de flottaison.
 		Buoyancy (Time.fixedDeltaTime);
+		//On appelle l'anim de tanguage.
 		Pendulum (Time.fixedDeltaTime);
+		//On appelle l'anim d'inclinaison.
 		ForwardTilt (Time.fixedDeltaTime);
 
+		//On modifie la rotation du bateau grâce aux données calculées.
 		transform.localEulerAngles = new Vector3(currentForwardTiltAngle, transform.localEulerAngles.y, currentSideAngle);
 	}
 
+	//Fonction gérant la flottaison.
 	void Buoyancy (float timeTick) {
-		currentBuoyancyLength = Mathf.Lerp (restLength, fastSpeedLenght, currentSpeedFactor);
-		currentBuoyancyAnimationPoint += timeTick / currentBuoyancyLength;
-		currentBuoyancyAnimationPoint %= 1;
+		//On calcule l'avancée dans le cycle de l'animation.
+		currentBuoyancyLength = Mathf.Lerp (restLength, fastSpeedLenght, currentSpeedFactor); //D'abord on trouve la durée actuelle par rapport à la vitesse.
+		currentBuoyancyAnimationPoint += timeTick / currentBuoyancyLength; //Ensuite on augmente l'avancée dans le cycle.
+		currentBuoyancyAnimationPoint %= 1; //Et enfin on cap le cycle pour qu'il boucle entre 0 et 1.
 
+		//On calcule l'amplitude actuel de l'animation.
 		currentBuoyancyStrength = Mathf.Lerp (restStrength, fastSpeedStrenght, currentSpeedFactor);
 
+		//On maintenant recupérer la valeur lambda à ce moment du cycle et la multiplié par l'ampleur.
 		currentBuoyancyValue = restCurve.Evaluate (currentBuoyancyAnimationPoint) * currentBuoyancyStrength;
+		//Et on applique bien sûr.
 		Vector3 pos = transform.position;
 		pos.y = currentBuoyancyValue;
 		transform.position = pos;
 	}
 
+	//Fonction gérant la tanguage.
 	void Pendulum (float timeTick) {
+		//Le tanguage se comporte différement si le stick est au point neutre ou non.
 		if (aimedSideAngle != 0) {
-			float sign = Mathf.Sign(aimedSideAngle);
-			float factor = 1 - (currentSideAngle / aimedSideAngle);
-			currentSideAngularVelocity += factor * sign * sideSpeed * timeTick;
+			//Si le stick n'est pas au point neutre, on gagne de la vélocité pour rejoindre le point visé.
+			float sign = Mathf.Sign(aimedSideAngle); //On recupère le signe pour aller dans le bon sens.
+			float factor = 1 - (currentSideAngle / aimedSideAngle); //Ce truc vas servir à modeler une force de résistance.
+			currentSideAngularVelocity += factor * sign * sideSpeed * timeTick; //On ajoute la vélocité.
 		}
 		else {
-			float sign = (currentSideAngle != 0 ) ? Mathf.Sign(currentSideAngle) : 0;
-			currentSideAngularVelocity -= sideGravity * timeTick * sign;
+			//Si le stick est au point neutre, il doit y revenir et tanguer avant de se stabiliser.
+			float sign = (currentSideAngle != 0 ) ? Mathf.Sign(currentSideAngle) : 0; //On récupère le signe de l'angle pour appliquer la gravité dans le sens inverse.
+			currentSideAngularVelocity -= sideGravity * timeTick * sign; //On ajoute la vélocité.
 		}
 
-		currentSideAngularVelocity *= (1 - Time.fixedDeltaTime * sideDrag); //Drag Formula ! Fucking useful !
-		currentSideAngle += currentSideAngularVelocity * timeTick;
+		currentSideAngularVelocity *= (1 - Time.fixedDeltaTime * sideDrag); //Formule permettant de calculer le drag sur la vélocité. En gros la résistance de l'air pour qu'il n'y est pas de mouvement infini.
+		currentSideAngle += currentSideAngularVelocity * timeTick; //On fait bouger l'angle grâce à la vélocité angulaire.
 	}
 
+	//Fonction gérant l'inclinaison.
 	void ForwardTilt (float timeTick) {
+		//On choppe l'amplitude d'inclinaison en fonction de la vitesse.
 		currentForwardTiltStrength = Mathf.Lerp (0, fastSpeedTiltStrength, currentSpeedFactor);
-
+		//On récupère la valeur de base à ce point du cycle et la multiplie à l'amplitude.
+		//Pas besoin de plus puisqu'on se base sur le cycle de flottaison et qu'on a tout calculé avant.
 		currentForwardTiltAngle = fastSpeedTiltCurve.Evaluate (currentBuoyancyAnimationPoint) * currentForwardTiltStrength;
 
+		//Permet de jouer le deuxième son d'eau.
 		if ((currentBuoyancyAnimationPoint >= waterTimeStamp) && _waterCanPlay) {
 			MasterAudio.PlaySound3DAtVector3AndForget (waterSound, transform.position, waterVolume.Evaluate ((_rb.velocity.magnitude > fastSpeedReference) ? 1 : _rb.velocity.magnitude / fastSpeedReference));
 			_waterCanPlay = false;
@@ -128,7 +166,9 @@ public class FloatingObjectAnimations : MonoBehaviour {
 		}
 	}
 
+	//La fonction permettant au script du Bateau de fixer l'angle visée pour le tanguage (en fonction de la force de rotation).
 	public void SetSideMovement (float force) {
-		aimedSideAngle = -force * sideStrength;
+		//On fait un calcul simple en passant la force dans un facteur pour obtenir l'angle visée.
+		aimedSideAngle = -force * sideStrength; 
 	}
 }
